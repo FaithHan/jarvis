24c36f317bb029ad6ff397a09ac5f371a388b7ad
hanfei08 <hanfei08@baidu.com>
2021-09-15 00:11:55 +0800

    add aes utils

---
+++ src/main/java/org/jarvis/codec/AESUtils.java
@@ -0,0 +1,77 @@
+package org.jarvis.codec;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jarvis.misc.HexUtils;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * BouncyCastle:
+ * https://segmentfault.com/a/1190000023445325 <p>
+ * GCM模式
+ * https://cloud.tencent.com/developer/article/1161339
+ * 随机序列固定
+ * SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+ */
+@Slf4j
+public class AESUtils {
+
+    private static final String ENCRY_ALGORITHM = "AES";
+    /**
+     * 加密算法/加密模式/填充类型
+     * 本例采用AES加密，ECB加密模式，PKCS5Padding填充
+     */
+    private static final String CIPHER_MODE = "AES/ECB/PKCS5Padding";
+
+    public static byte[] generateAESKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            // 128 or 192 or 256
+            keyGenerator.init(128);
+            SecretKey secretKey = keyGenerator.generateKey();
+            return secretKey.getEncoded();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static byte[] encrypt(byte[] clearTextBytes, byte[] keyBytes) {
+        try {
+            // 1 获取加密密钥
+            SecretKeySpec keySpec = new SecretKeySpec(keyBytes, ENCRY_ALGORITHM);
+            // 2 获取Cipher实例
+            Cipher cipher = Cipher.getInstance(CIPHER_MODE);
+            // 3 初始化Cipher实例。设置执行模式以及加密密钥
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            // 4 执行并返回密文字符集
+            return cipher.doFinal(clearTextBytes);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static byte[] decrypt(byte[] cipherTextBytes, byte[] keyBytes) {
+        try {
+            // 1 获取解密密钥
+            SecretKeySpec keySpec = new SecretKeySpec(keyBytes, ENCRY_ALGORITHM);
+
+            // 2 获取Cipher实例
+            Cipher cipher = Cipher.getInstance(CIPHER_MODE);
+
+            // 3 初始化Cipher实例。设置执行模式以及加密密钥
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
+
+            //4 执行并返回明文字符集
+            return cipher.doFinal(cipherTextBytes);
+
+        } catch (Exception e) {
+            log.error("AES解密错误", e);
+        }
+        return null;
+    }
+}
---
+++ src/test/java/org/jarvis/codec/AESUtilsTest.java
@@ -0,0 +1,28 @@
+package org.jarvis.codec;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AESUtilsTest {
+
+    @Test
+    void generateAESKey() {
+        byte[] bytes = AESUtils.generateAESKey();
+        assertEquals(16, bytes.length);
+    }
+
+    @Test
+    void encrypt_decrypt_test() {
+        byte[] bytes = AESUtils.generateAESKey();
+        byte[] cipherTextBytes = AESUtils.encrypt("你好".getBytes(StandardCharsets.UTF_8), bytes);
+        byte[] clearTextBytes = AESUtils.decrypt(cipherTextBytes, bytes);
+        String clearText = new String(clearTextBytes, StandardCharsets.UTF_8);
+        Assertions.assertEquals("你好",clearText);
+
+    }
+
+}
