9da4c5e6374472a7e19743a01486c1804ae04518
hanfei08 <hanfei08@baidu.com>
2021-09-21 09:35:27 +0800

    add Hmac test

---
+++ src/test/java/org/jarvis/security/crypto/mac/HMACProviderTest.java
@@ -0,0 +1,17 @@
+package org.jarvis.security.crypto.mac;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+class HMACProviderTest {
+
+    @Test
+    void hmacHex() {
+        HMACProvider hmacProvider = new HMACProvider(HmacFunction.HMAC_SHA_256, "nihao");
+        String result1 = hmacProvider.hmacHex("百度网讯技术有限公司".getBytes(StandardCharsets.UTF_8));
+        String result2 = hmacProvider.hmacHex("百度网讯技术有限公司".getBytes(StandardCharsets.UTF_8));
+        Assertions.assertEquals(result1, result2);
+    }
+}
