fb8bac7a8f817d85fea4267bb1e99c9b89bed642
hanfei08 <hanfei08@baidu.com>
2021-10-06 21:11:18 +0800

    update RedisUtils

--- src/main/java/org/jarvis/redis/RedisUtils.java
+++ src/main/java/org/jarvis/redis/RedisUtils.java
@@ -9,6 +9,10 @@ import java.time.Instant;
 
 public class RedisUtils {
 
+    public static StringRedisTemplate getStringRedisTemplate(int port) {
+        return getStringRedisTemplate("127.0.0.1", port);
+    }
+
     public static StringRedisTemplate getStringRedisTemplate(String host, int port) {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
         LettuceConnectionFactory fac = new LettuceConnectionFactory(host, port);
@@ -18,11 +22,11 @@ public class RedisUtils {
         stringRedisTemplate.afterPropertiesSet();
         long l = Instant.now().toEpochMilli();
         stringRedisTemplate.execute((RedisCallback<Object>) RedisConnectionCommands::ping);
-        System.out.println(Instant.now().toEpochMilli() - l);
+//        System.out.println(Instant.now().toEpochMilli() - l);
 
         l = Instant.now().toEpochMilli();
         stringRedisTemplate.execute((RedisCallback<Object>) RedisConnectionCommands::ping);
-        System.out.println(Instant.now().toEpochMilli() - l);
+//        System.out.println(Instant.now().toEpochMilli() - l);
         return stringRedisTemplate;
     }
 }
