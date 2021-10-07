06ad53215b511d21d15fd039c821df6a977e2f75
hanfei08 <hanfei08@baidu.com>
2021-10-07 10:26:41 +0800

    update SimpleRateLimiterTest

--- src/test/java/org/jarvis/redis/ratelimiter/SimpleRateLimiterTest.java
+++ src/test/java/org/jarvis/redis/ratelimiter/SimpleRateLimiterTest.java
@@ -1,17 +1,13 @@
 package org.jarvis.redis.ratelimiter;
 
-import com.google.common.util.concurrent.RateLimiter;
 import org.jarvis.clock.Stopwatch;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleRateLimiterTest {
 
     @Test
     void acquire() {
         SimpleRateLimiter rateLimiter = new SimpleRateLimiter(1, 1);
-//        RateLimiter rateLimiter = RateLimiter.create(100);
         Stopwatch stopwatch = Stopwatch.createStarted();
         for (int i = 0; i < 10; i++) {
             rateLimiter.acquire(1);
