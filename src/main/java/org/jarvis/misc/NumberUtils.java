ed5769d5947bd4daf3186ddb060aa70500d4cfeb
hanfei08 <hanfei08@baidu.com>
2021-10-11 22:57:49 +0800

    add NumberUtils

---
+++ src/main/java/org/jarvis/misc/NumberUtils.java
@@ -0,0 +1,100 @@
+package org.jarvis.misc;
+
+import java.lang.reflect.Array;
+
+public abstract class NumberUtils {
+
+    public static int min(int... array) {
+        validateArray(array);
+        int min = array[0];
+        for (int number : array) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    public static long min(long... array) {
+        validateArray(array);
+        long min = array[0];
+        for (long number : array) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    public static float min(float... array) {
+        validateArray(array);
+        float min = array[0];
+        for (float number : array) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    public static double min(double... array) {
+        validateArray(array);
+        double min = array[0];
+        for (double number : array) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    public static int max(int... array) {
+        validateArray(array);
+        int max = array[0];
+        for (int number : array) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public static long max(long... array) {
+        validateArray(array);
+        long max = array[0];
+        for (long number : array) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public static float max(float... array) {
+        validateArray(array);
+        float max = array[0];
+        for (float number : array) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public static double max(double... array) {
+        validateArray(array);
+        double max = array[0];
+        for (double number : array) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    private static void validateArray(Object array) {
+        Assert.notNull(array, "intArray can not be null.");
+        Assert.isTrue(Array.getLength(array) != 0, "intArray cannot be empty.");
+    }
+
+}
