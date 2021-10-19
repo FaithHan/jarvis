4d3383c9bc1c1269a0bf4baa57962e2305929991
hanfei08 <hanfei08@baidu.com>
2021-10-19 20:10:20 +0800

    update IOUtils

--- src/main/java/org/jarvis/io/IOUtils.java
+++ src/main/java/org/jarvis/io/IOUtils.java
@@ -1,5 +1,7 @@
 package org.jarvis.io;
 
+import org.apache.commons.io.output.NullOutputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,7 +11,7 @@ import java.io.Writer;
 public abstract class IOUtils {
 
 
-    public void closeSilent(InputStream inputStream) {
+    public static void closeSilent(InputStream inputStream) {
         if (inputStream != null) {
             try {
                 inputStream.close();
@@ -19,7 +21,7 @@ public abstract class IOUtils {
         }
     }
 
-    public void closeSilent(OutputStream outputStream) {
+    public static void closeSilent(OutputStream outputStream) {
         if (outputStream != null) {
             try {
                 outputStream.close();
@@ -29,7 +31,7 @@ public abstract class IOUtils {
         }
     }
 
-    public void closeSilent(Writer writer) {
+    public static void closeSilent(Writer writer) {
         if (writer != null) {
             try {
                 writer.close();
@@ -39,7 +41,7 @@ public abstract class IOUtils {
         }
     }
 
-    public void closeSilent(Reader reader) {
+    public static void closeSilent(Reader reader) {
         if (reader != null) {
             try {
                 reader.close();
@@ -47,6 +49,15 @@ public abstract class IOUtils {
 
             }
         }
+    }
+
+    public static void consume(InputStream inputStream) throws IOException {
+        StreamCopyUtils.copy(inputStream, new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+
+            }
+        });
     }
 
 
