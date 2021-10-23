a4f0b363bc804eebb7754e21ba62a0210bce2b19
hanfei08 <hanfei08@baidu.com>
2021-10-23 22:50:05 +0800

    add DESUtilsTest

---
+++ src/test/java/org/jarvis/security/crypto/DESUtilsTest.java
@@ -0,0 +1,20 @@
+package org.jarvis.security.crypto;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DESUtilsTest {
+
+
+    @Test
+    void encrypt() {
+        byte[] bytes = DESUtils.generateAESKey();
+        byte[] cipherTextBytes = DESUtils.encrypt("你好".getBytes(StandardCharsets.UTF_8), bytes);
+        byte[] clearTextBytes = DESUtils.decrypt(cipherTextBytes, bytes);
+        String clearText = new String(clearTextBytes, StandardCharsets.UTF_8);
+        assertEquals("你好",clearText);
+    }
+}
