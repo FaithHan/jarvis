0aec4fd9d65f09c4e76f9b9e75a0a02978369874
hanfei08 <hanfei08@baidu.com>
2021-10-26 10:01:50 +0800

    add PartitionTest

---
+++ src/test/java/org/jarvis/misc/PartitionTest.java
@@ -0,0 +1,22 @@
+package org.jarvis.misc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartitionTest {
+
+    @Test
+    void test() {
+        List<Integer> collect = IntStream.rangeClosed(1, 105).boxed().collect(Collectors.toList());
+        Partition<Integer> partition = Partition.partition(collect, 10);
+        for (List<Integer> integers : partition) {
+            System.out.println(integers);
+        }
+        assertEquals(11, partition.size());
+    }
+}
