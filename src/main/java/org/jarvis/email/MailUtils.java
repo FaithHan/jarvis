4e54873e14cce50381cb2c39145f8efd3cb43978
hanfei08 <hanfei08@baidu.com>
2021-10-31 17:51:56 +0800

    update MailUtils

--- src/main/java/org/jarvis/email/MailUtils.java
+++ src/main/java/org/jarvis/email/MailUtils.java
@@ -1,5 +1,9 @@
 package org.jarvis.email;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jarvis.io.StreamCopyUtils;
+import org.jarvis.misc.Assert;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -7,22 +11,36 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.Date;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * https://www.baeldung.com/spring-email
+ * https://javaee.github.io/javamail/
+ * https://www.javatpoint.com/java-mail-api-tutorial
+ * https://www.baeldung.com/java-email
+ */
+@Slf4j
 public class MailUtils {
 
-    private static final JavaMailSender javaMailSender;
+    private static final String DEFAULT_CONTENT_TYPE = "application/octet-stream";
+
+    private static final String[] EMPTY_ARRAY = new String[0];
+
+    private static final JavaMailSender SENDER;
+
+    public static String from = "hanfei08@baidu.com";
 
     static {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("mail.baidu.com");
         mailSender.setPort(25);
-        mailSender.setUsername("xxxx");
-        mailSender.setPassword("xxxx");
+        mailSender.setUsername("hanfei08");
+        mailSender.setPassword("*****");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
@@ -30,51 +48,107 @@ public class MailUtils {
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
-        javaMailSender = mailSender;
+        SENDER = mailSender;
     }
 
-    public void sendSimpleMail(String[] to, String[] cc, String subject, String text) {
+    public void sendSimpleMail(String[] to, String[] cc, String[] bcc, String subject, String content) {
+        Assert.notNull(to, "to array can not be null");
+        Assert.notNull(subject, "subject can not be null");
+        Assert.notNull(content, "content can not be null");
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("xxxx@baidu.com");
+        message.setFrom(from);
         message.setTo(to);
-        message.setCc(cc);
+        message.setCc(cc != null ? cc : EMPTY_ARRAY);
+        message.setBcc(bcc != null ? bcc : EMPTY_ARRAY);
         message.setSubject(subject);
-        message.setText(text);
-        javaMailSender.send(message);
+        message.setText(content);
+        SENDER.send(message);
     }
 
-    public static void sendMailWithAttachment(String[] to, String[] cc, String subject,
-                                              String text, List<File> attachments, Map<String, File> inlineMap) {
+    public static void sendTextMail(String[] to, String[] cc, String[] bcc,
+                                    String subject, String content, PriorityEnum priority,
+                                    Map<String, InputStream> attachmentMap) {
+        sendMail(to, cc, bcc, subject, content, priority, attachmentMap, null, false);
+    }
 
-        MimeMessage message = javaMailSender.createMimeMessage();
+    public static void sendHtmlMail(String[] to, String[] cc, String[] bcc,
+                                    String subject, String content, PriorityEnum priority,
+                                    Map<String, InputStream> attachmentMap, Map<String, InputStream> inlineMap) {
+        sendMail(to, cc, bcc, subject, content, priority, attachmentMap, inlineMap, true);
+    }
 
+    /**
+     * send isHTML email
+     *
+     * @param to
+     * @param cc
+     * @param bcc
+     * @param subject
+     * @param content
+     * @param priority      PriorityEnum
+     * @param attachmentMap 附件 文件名-输入流
+     * @param inlineMap     资源 cuid - 输入流 例：<img class="avator" src="cid:img1">
+     * @param isHTML        is HTML mode?
+     */
+    private static void sendMail(String[] to, String[] cc, String[] bcc,
+                                 String subject, String content, PriorityEnum priority,
+                                 Map<String, InputStream> attachmentMap, Map<String, InputStream> inlineMap,
+                                 boolean isHTML) {
+        Assert.notNull(to, "to array can not be null");
+        Assert.notNull(subject, "subject can not be null");
+        Assert.notNull(content, "content can not be null");
         try {
-            MimeMessageHelper helper =
-                    new MimeMessageHelper(message, true, StandardCharsets.UTF_8.displayName());
-            helper.setFrom("xxxx@baidu.com");
+            MimeMessage message = SENDER.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+            helper.setFrom(from);
             helper.setTo(to);
-            helper.setCc(cc);
+            helper.setCc(cc != null ? cc : EMPTY_ARRAY);
+            helper.setCc(bcc != null ? bcc : EMPTY_ARRAY);
             helper.setSubject(subject);
-            helper.setText(text, true);
-            attachments.forEach(file -> {
-                try {
-                    helper.addAttachment(file.getName(), file);
-                } catch (MessagingException ignored) {
-
+            helper.setText(content, isHTML);
+            helper.setSentDate(new Date());
+            helper.setEncodeFilenames(true); // 解决附件中文名称乱码
+            switch (priority) {
+                case LOW:
+                    helper.setPriority(5);
+                    break;
+                case HIGH:
+                    helper.setPriority(1);
+                    break;
+                case NORMAL:
+                default:
+            }
+            if (attachmentMap != null) {
+                for (Map.Entry<String, InputStream> entry : attachmentMap.entrySet()) {
+                    String name = entry.getKey();
+                    InputStream inputStream = entry.getValue();
+                    helper.addAttachment(name, new ByteArrayResource(StreamCopyUtils.copyToByteArray(inputStream)));
                 }
-            });
-            inlineMap.forEach((cid,file) -> {
-                try {
-                    helper.addInline(cid, file);
-                } catch (MessagingException ignored) {
-
+            }
+            if (isHTML && inlineMap != null) {
+                for (Map.Entry<String, InputStream> entry : inlineMap.entrySet()) {
+                    String cuid = entry.getKey();
+                    InputStream inputStream = entry.getValue();
+                    helper.addInline(cuid, new ByteArrayResource(StreamCopyUtils.copyToByteArray(inputStream)), DEFAULT_CONTENT_TYPE);
                 }
-            });
-        } catch (MessagingException e) {
+            }
+            sendMessage(message);
+        } catch (MessagingException | IOException e) {
+            log.error("send mail error", e);
             throw new RuntimeException(e);
         }
+    }
 
-        javaMailSender.send(message);
+    private static void sendMessage(MimeMessage mimeMessage) {
+        SENDER.send(mimeMessage);
+    }
+
+    private static void sendMessage(SimpleMailMessage simpleMailMessage) {
+        SENDER.send(simpleMailMessage);
+    }
+
+    public enum PriorityEnum {
+        LOW, NORMAL, HIGH
     }
 
 }
--- src/test/java/org/jarvis/email/MailUtilsTest.java
+++ src/test/java/org/jarvis/email/MailUtilsTest.java
@@ -3,22 +3,32 @@ package org.jarvis.email;
 import org.jarvis.io.ClassPathUtils;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.util.Collections;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MailUtilsTest {
 
     @Test
-    void sendMailWithAttachment() {
-        MailUtils.sendMailWithAttachment(new String[]{"xxxx"},
-                new String[]{},
-                "Hello World", ClassPathUtils.copyToString("template.html"), Collections.emptyList(),
-                new HashMap<String, File>(){
+    void sendMailWithAttachment() throws FileNotFoundException {
+        MailUtils.sendHtmlMail(
+                new String[]{"****@qq.com"},
+//                new String[]{"freepuresakura@gmail.com"},
+                null,
+                null,
+                "你好世界",
+                ClassPathUtils.copyToString("mail_template.html"),
+                MailUtils.PriorityEnum.NORMAL,
+                new HashMap<String, InputStream>(){
                     {
-                        put("img1", new File(""));
+                        put("头像哈哈", new FileInputStream("/Users/hanfei08/Desktop/131628246419_.pic_thumb.jpg"));
+                    }
+                },
+                new HashMap<String, InputStream>(){
+                    {
+                        put("img1", new FileInputStream("/Users/hanfei08/Desktop/131628246419_.pic_thumb.jpg"));
+                        put("video1", new FileInputStream("/Users/hanfei08/Downloads/1631602998476450.mp4"));
                     }
                 }
         );
