64da7f4b78da98c102c71cfc1f764c9082759fb2
hanfei08 <hanfei08@baidu.com>
2022-02-17 10:20:28 +0800

    SignatureAlgorithm add comment for RS_256_PSS

--- src/main/java/org/jarvis/security/crypto/signature/SignatureAlgorithm.java
+++ src/main/java/org/jarvis/security/crypto/signature/SignatureAlgorithm.java
@@ -5,6 +5,12 @@ public class SignatureAlgorithm {
 
     public static final String RS_256 = "SHA256withRSA";
 
+    /**
+     * https://en.wikipedia.org/wiki/PKCS_1
+     * https://blog.csdn.net/chenzhi5174/article/details/100719023
+     * https://www.cryptosys.net/pki/manpki/pki_rsaschemes.html
+     * http://web.archive.org/web/20040713140300/http://grouper.ieee.org/groups/1363/P1363a/contributions/pss-submission.pdf
+     */
     public static final String RS_256_PSS = "SHA256withRSA/PSS";
 
     public static final String RS_384 = "SHA384withRSA";
