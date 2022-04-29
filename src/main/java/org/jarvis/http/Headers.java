d737ad29a69acdc1cf3ac32ae7c08d24e2113ce1
hanfei08 <hanfei08@baidu.com>
2022-04-29 16:46:34 +0800

    add basic auth method

--- src/main/java/org/jarvis/http/Headers.java
+++ src/main/java/org/jarvis/http/Headers.java
@@ -1,6 +1,13 @@
 package org.jarvis.http;
 
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +23,10 @@ public class Headers extends HashMap<String, List<String>> {
     public Headers addHeaders(Headers headers) {
         headers.forEach((headerName, headerValues) -> computeIfAbsent(headerName, key -> new ArrayList<>()).addAll(headerValues));
         return this;
+    }
+
+    public Headers addBasicAuth(String username, String password) {
+        return addHeader("Authorization", "Basic " + encodeBasicAuth(username, password, StandardCharsets.UTF_8));
     }
 
     /**
@@ -45,5 +56,24 @@ public class Headers extends HashMap<String, List<String>> {
     @Override
     public void clear() {
         super.clear();
+    }
+
+    private static String encodeBasicAuth(String username, String password, @Nullable Charset charset) {
+        Assert.notNull(username, "Username must not be null");
+        Assert.doesNotContain(username, ":", "Username must not contain a colon");
+        Assert.notNull(password, "Password must not be null");
+        if (charset == null) {
+            charset = StandardCharsets.ISO_8859_1;
+        }
+
+        CharsetEncoder encoder = charset.newEncoder();
+        if (!encoder.canEncode(username) || !encoder.canEncode(password)) {
+            throw new IllegalArgumentException(
+                    "Username or password contains characters that cannot be encoded to " + charset.displayName());
+        }
+
+        String credentialsString = username + ":" + password;
+        byte[] encodedBytes = Base64.getEncoder().encode(credentialsString.getBytes(charset));
+        return new String(encodedBytes, charset);
     }
 }
